d548e1cce3d3524114a2a39f23e1288527c44af9
RelebohileHlabana <relebohilehlabana96@gmail.com>
2023-09-17 14:54:05 -0700

    printf.c

--- printf.c
+++ printf.c
@@ -10,51 +10,80 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int printed_chars = 0;
+	va_list args;
+	int printed_chars = 0;
 
-    va_start(args, format);
+	va_start(args, format);
+	printed_chars = process_format_string(format, args);
+	va_end(args);
 
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            _putchar(*format);
-            printed_chars++;
-        }
-        else
-        {
-            format++;
+	return (printed_chars);
+}
 
-            if (*format == '\0')
-                break;
+/**
+ * process_format_string - Process the format string and print output
+ * @format: The format string
+ * @args: The variable arguments list
+ *
+ * Return: The number of characters printed
+ */
+int process_format_string(const char *format, va_list args)
+{
+	int printed_chars = 0;
 
-            switch (*format)
-            {
-                case 'c':
-                    _putchar(va_arg(args, int));
-                    printed_chars++;
-                    break;
-                case 's':
-                    printed_chars += _print_str(va_arg(args, char *));
-                    break;
-                case '%':
-                    _putchar('%');
-                    printed_chars++;
-                    break;
-                default:
-                    _putchar('%');
-                    _putchar(*format);
-                    printed_chars += 2;
-                    break;
-            }
-        }
-        format++;
-    }
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			_putchar(*format);
+			printed_chars++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			printed_chars += process_conversion_specifier(*format, args);
+		}
+		format++;
+	}
 
-    va_end(args);
+	return (printed_chars);
+}
 
-    return printed_chars;
+/**
+ * process_conversion_specifier - Process a conversion specifier
+ * @specifier: The conversion specifier character
+ * @args: The variable arguments list
+ *
+ * Return: The number of characters printed
+ */
+int process_conversion_specifier(char specifier, va_list args)
+{
+	int printed_chars = 0;
+
+	switch (specifier)
+	{
+		case 'c':
+			_putchar(va_arg(args, int));
+			printed_chars++;
+			break;
+		case 's':
+			printed_chars += _print_str(va_arg(args, char *));
+			break;
+		case '%':
+			_putchar('%');
+			printed_chars++;
+			break;
+		default:
+			_putchar('%');
+			_putchar(specifier);
+			printed_chars += 2;
+			break;
+	}
+
+	return (printed_chars);
+
 }
 
 /**
@@ -65,17 +94,20 @@ int _printf(const char *format, ...)
  */
 int _print_str(char *str)
 {
-    int printed_chars = 0;
+	int printed_chars = 0;
 
-    if (str == NULL)
-        str = "(null)";
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
 
-    while (*str)
-    {
-        _putchar(*str);
-        str++;
-        printed_chars++;
-    }
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+		printed_chars++;
+	}
 
-    return printed_chars;
+	return (printed_chars);
 }
+
