4b9011cd613460b3db4176776edb229071039509
Ericeva4 <ericeva44@gmail.com>
2023-09-18 07:59:23 -0700

    printf

--- main.h
+++ main.h
@@ -9,6 +9,7 @@ int process_conversion_specifier(char specifier, va_list args);
 int _print_str(char *str);
 int _putchar(char c);
 int _print_custom_specifier(char *str);
+int sizes(const char *our_format, int *j);
 
 #endif /* MAIN_H */
 
